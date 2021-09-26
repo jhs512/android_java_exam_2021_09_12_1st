449cad183684e383faffb7955531527e899cf1e9
jhs512 <jangka512@gmail.com>
2021-09-26 17:13:06 +0900

    공통적으로 사용된 지역변수를 인스턴스화

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,22 +9,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editTextNum1;
+    EditText editTextNum2;
+    TextView textViewRs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextNum1 = findViewById(R.id.activity_main__editTextNum1);
+        editTextNum2 = findViewById(R.id.activity_main__editTextNum2);
+        textViewRs = findViewById(R.id.activity_main__textViewRs);
     }
 
     public void btnAddClicked(View view) {
-        EditText editTextNum1 = findViewById(R.id.activity_main__editTextNum1);
-        EditText editTextNum2 = findViewById(R.id.activity_main__editTextNum2);
-        TextView textViewRs = findViewById(R.id.activity_main__textViewRs);
-
         // 혹시나 있을지 모를 양옆의 공백제거
         editTextNum1.setText(editTextNum1.getText().toString().trim());
 
-        if ( editTextNum1.getText().toString().length() == 0 ) {
+        if (editTextNum1.getText().toString().length() == 0) {
             toastMsg("숫자1을 입력해주세요.");
             editTextNum1.requestFocus();
             return;
@@ -33,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         // 혹시나 있을지 모를 양옆의 공백제거
         editTextNum2.setText(editTextNum2.getText().toString().trim());
 
-        if ( editTextNum2.getText().toString().length() == 0 ) {
+        if (editTextNum2.getText().toString().length() == 0) {
             toastMsg("숫자2를 입력해주세요.");
             editTextNum2.requestFocus();
             return;
@@ -47,29 +50,11 @@ public class MainActivity extends AppCompatActivity {
         textViewRs.setText("결과 : " + rs);
     }
 
-    void toastMsg(String msg) {
-        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
-    }
-
-    public void btnClearClicked(View view) {
-        EditText editTextNum1 = findViewById(R.id.activity_main__editTextNum1);
-        EditText editTextNum2 = findViewById(R.id.activity_main__editTextNum2);
-        TextView textViewRs = findViewById(R.id.activity_main__textViewRs);
-
-        editTextNum1.setText("");
-        editTextNum2.setText("");
-        textViewRs.setText("");
-    }
-
     public void btnMinusClicked(View view) {
-        EditText editTextNum1 = findViewById(R.id.activity_main__editTextNum1);
-        EditText editTextNum2 = findViewById(R.id.activity_main__editTextNum2);
-        TextView textViewRs = findViewById(R.id.activity_main__textViewRs);
-
         // 혹시나 있을지 모를 양옆의 공백제거
         editTextNum1.setText(editTextNum1.getText().toString().trim());
 
-        if ( editTextNum1.getText().toString().length() == 0 ) {
+        if (editTextNum1.getText().toString().length() == 0) {
             toastMsg("숫자1을 입력해주세요.");
             editTextNum1.requestFocus();
             return;
@@ -78,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         // 혹시나 있을지 모를 양옆의 공백제거
         editTextNum2.setText(editTextNum2.getText().toString().trim());
 
-        if ( editTextNum2.getText().toString().length() == 0 ) {
+        if (editTextNum2.getText().toString().length() == 0) {
             toastMsg("숫자2를 입력해주세요.");
             editTextNum2.requestFocus();
             return;
@@ -90,5 +75,15 @@ public class MainActivity extends AppCompatActivity {
         int rs = num1 - num2;
         toastMsg("결과 : " + rs);
         textViewRs.setText("결과 : " + rs);
+    }
+
+    public void btnClearClicked(View view) {
+        editTextNum1.setText("");
+        editTextNum2.setText("");
+        textViewRs.setText("결과");
+    }
+
+    void toastMsg(String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
 }
